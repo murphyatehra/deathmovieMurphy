a34a73ded2b27ef099e542023f5bcb7f38f589c1
deathehra <jarvidniies@gmail.com>
2025-08-04 16:00:53 +0530

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import Homepage from '@/components/Homepage';
+
+const Index = () => {
+  console.log('Index page rendering with Homepage component');
+  
+  return <Homepage />;
+};
+
+export default Index;
